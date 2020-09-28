6a69cb66a74f0247e5619673a07ffc2048008e9b
felipe-escalhao <escalhao3@gmail.com>
2020-09-28 19:58:23 -0300

    DockerCourse

---
+++ simpleweb/index.js
@@ -0,0 +1,11 @@
+const express = required('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi there');
+});
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080');
+});
